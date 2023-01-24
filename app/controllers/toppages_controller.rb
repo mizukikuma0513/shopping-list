a495226b17903994e128dabc286da6862457951d
mizuki.kondou <mizukikuma0513@gmail.com>
2023-01-24 14:39:04 +0000

    post

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,9 @@
 class ToppagesController < ApplicationController
   def index
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      #@number = current_user.tasks.build # form_with 用
+      @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_24_090347) do
+ActiveRecord::Schema.define(version: 2023_01_24_141845) do
+
+  create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
+    t.string "content"
+    t.string "number"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
@@ -20,4 +29,5 @@ ActiveRecord::Schema.define(version: 2023_01_24_090347) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "tasks", "users"
 end
